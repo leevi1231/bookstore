10ff284d3284e9d709b67142cd30d12e0ca56ab4
leevi1231 <148748086+leevi1231@users.noreply.github.com>
2024-03-10 16:40:14 +0200

    BookRestController jne

--- bookstore/src/main/java/bookstore/leevi1231/bookstore/domain/Category.java
+++ bookstore/src/main/java/bookstore/leevi1231/bookstore/domain/Category.java
@@ -2,6 +2,8 @@ package bookstore.leevi1231.bookstore.domain;
 
 import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -16,6 +18,7 @@ public class Category {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @JsonIgnore
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "category")
     private List<Book> books;
 
